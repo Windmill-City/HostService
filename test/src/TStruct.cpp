1253aae7c593a37d843affdad23bcd1c4f0f18ad
Windmill_City <1449182174@qq.com>
2024-04-04 10:02:18 +0800

    1. 增添字符串容器

--- test/src/TStruct.cpp
+++ test/src/TStruct.cpp
@@ -13,6 +13,20 @@ TEST(Struct, sizeof)
     EXPECT_EQ(sizeof(Struct<FloatSt>), 8);
 }
 
+TEST(String, Use)
+{
+    String<32> str_1 = "Hello World";
+    String<32> str_2 = "Hello World";
+
+    // 测试赋值操作
+    str_1            = "Hello";
+    EXPECT_STREQ(str_1, "Hello");
+
+    // 测试互相赋值
+    str_1 = str_2 = "World";
+    EXPECT_STREQ(str_1, str_2);
+}
+
 TEST_F(HostCS, Struct_GetProperty)
 {
     Struct<FloatSt> prop;
