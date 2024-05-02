fc2695ff4684748d6400e668a4980ed405425c76
Windmill_City <1449182174@qq.com>
2024-05-02 20:44:54 +0800

    1. 移除无用的宏定义

--- include/AES.hpp
+++ include/AES.hpp
@@ -1,11 +1,8 @@
 #pragma once
 #include <array>
 
-#define NONCE_SIZE_IN_BYTE 12
-#define AES_KEY_SIZE_IN_BIT 256
-
 struct AES
 {
-    std::array<uint8_t, NONCE_SIZE_IN_BYTE>      Nonce;
-    std::array<uint8_t, AES_KEY_SIZE_IN_BIT / 8> Key;
+    std::array<uint8_t, 12>      Nonce;
+    std::array<uint8_t, 256 / 8> Key;
 };
