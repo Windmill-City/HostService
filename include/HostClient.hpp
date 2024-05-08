a270a7541ad2a33dc25e8d78d3b9f792f50fa9e7
Windmill_City <1449182174@qq.com>
2024-05-08 11:40:55 +0800

    1. 整理注释

--- include/HostClient.hpp
+++ include/HostClient.hpp
@@ -12,10 +12,7 @@ struct HostClient : public HostBase
     // 附加参数缓冲区
     Extra          _extra;
 
-    /* 轮询响应 */
     virtual bool   poll() override;
-    /* 发送请求帧 */
     void           send_request(const Command cmd, Extra& extra);
-    /* 接收响应帧 */
     bool           recv_response(Command& cmd, ErrorCode& err, Extra& extra);
 };
