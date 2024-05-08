ad8e5f93c89852fd5728a517b4c776a2859c542d
Windmill_City <1449182174@qq.com>
2024-05-08 14:29:58 +0800

    1. 修正静态初始化

--- test/src/THostCS.cpp
+++ test/src/THostCS.cpp
@@ -2,98 +2,75 @@
 #include <HostCS.hpp>
 #include <Property.hpp>
 
-TEST(HostClient, sizeof)
+TEST(Request, sizeof)
 {
-    EXPECT_EQ(sizeof(HostClient), 292);
+    ASSERT_EQ(sizeof(Request), 5);
 }
 
-TEST(HostServer, sizeof)
+TEST(Response, sizeof)
 {
-    HostServerImpl hs;
-    EXPECT_EQ(sizeof(HostServer), 356);
+    ASSERT_EQ(sizeof(Response), 6);
 }
 
-TEST_F(HostCS, request)
+TEST(HostServer, frozen)
 {
-    uint8_t data[] = {0x01, 0x02, 0x03};
+    // 验证 frozen map 是否能正常编译
+    static constinit HostServerImpl<1> Server({
+        {3, (PropertyBase*)8}
+    });
 
-    Extra   extra;
+    // 验证map大小是否正确
+    ASSERT_EQ(3, Server._props.size());
+    // 验证元素是否正确
+    ASSERT_EQ(8, (uint32_t)Server.get(3));
+}
+
+TEST(HostCS, request)
+{
+    HostCS<1> cs({
+        {1, (PropertyBase*)8}
+    });
+
+    uint8_t   data[] = {0x01, 0x02, 0x03};
+
+    Extra     extra;
     extra.add(data, sizeof(data));
-    client.send_request(Command::ECHO, extra);
+    cs.client.send_request(Command::ECHO, extra);
 
-    Poll();
+    cs.Poll();
 
-    EXPECT_TRUE(memcmp(client._extra.data(), data, sizeof(data)) == 0);
+    ASSERT_TRUE(memcmp(cs.client._extra.data(), data, sizeof(data)) == 0);
 }
 
-TEST_F(HostCS, ids_size)
+TEST(HostCS, nonce)
 {
-    PropertyId id = 0;
-    Extra      extra;
-    extra.add(id);
-    client.send_request(Command::GET_SIZE, extra);
+    HostCS     cs;
 
-    Poll();
-
-    client._extra.get(id);
-
-    uint16_t size;
-    client._extra.get(size);
-    EXPECT_EQ(size, sizeof(PropertyId) * 3);
-}
-
-TEST_F(HostCS, ids_content)
-{
-    // 添加一个属性值
-    Property<float> prop;
-    server.put(233, prop);
-
-    PropertyId   id = 0;
-
-    MemoryAccess access;
-    access.offset = sizeof(PropertyId) * 3;
-    access.size   = sizeof(PropertyId) * 1;
-
-    Extra extra;
-    extra.add(id);
-    extra.add(access);
-    client.send_request(Command::GET_MEMORY, extra);
-
-    Poll();
-
-    client._extra.get(id);
-    client._extra.get(access);
-
-    // id = 233, prop.float
-    client._extra.get(id);
-    EXPECT_EQ(id, 233);
-}
-
-TEST_F(HostCS, nonce)
-{
     PropertyId id = 1;
 
-    for (size_t i = 0; i < server.Nonce.ref().size(); i++)
+    for (size_t i = 0; i < cs.server.Nonce.ref().size(); i++)
     {
-        server.Nonce.ref()[i] = i;
+        cs.server.Nonce.ref()[i] = i;
     }
 
     Extra extra;
     extra.add(id);
-    client.send_request(Command::GET_PROPERTY, extra);
+    cs.client.send_request(Command::GET_PROPERTY, extra);
 
-    Poll();
+    cs.Poll();
 
-    client._extra.get(id);
+    cs.client._extra.get(id);
 
     std::array<uint8_t, 12> nonce;
-    EXPECT_EQ(nonce.size(), client._extra.remain());
-    client._extra.get(nonce.data(), nonce.size());
-    EXPECT_TRUE(memcmp(nonce.data(), server.Nonce.ref().data(), nonce.size()) == 0);
+    ASSERT_EQ(nonce.size(), cs.client._extra.remain());
+    cs.client._extra.get(nonce.data(), nonce.size());
+    ASSERT_TRUE(memcmp(nonce.data(), cs.server.Nonce.ref().data(), nonce.size()) == 0);
 }
 
-TEST_F(HostCS, key)
+TEST(HostCS, key)
 {
+    HostCS                       cs;
+
     PropertyId                   id = 2;
 
     std::array<uint8_t, 256 / 8> key;
@@ -107,10 +84,10 @@ TEST_F(HostCS, key)
     extra.reserve_tag();
     extra.add(id);
     extra.add(key.data(), key.size());
-    extra.encrypt(server.Nonce, server.Key);
-    client.send_request(Command::SET_PROPERTY, extra);
+    extra.encrypt(cs.server.Nonce, cs.server.Key);
+    cs.client.send_request(Command::SET_PROPERTY, extra);
 
-    Poll();
+    cs.Poll();
 
-    EXPECT_TRUE(memcmp(key.data(), server.Key.ref().data(), key.size()) == 0);
+    ASSERT_TRUE(memcmp(key.data(), cs.server.Key.ref().data(), key.size()) == 0);
 }
